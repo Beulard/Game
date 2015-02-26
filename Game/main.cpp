4f8d2a15c0ee8442055e71faa440df2cb33ff0cd
beulard <beulard@gmail.com>
2015-02-26 16:40:43 +0000

    Bim projection

--- Game/main.cpp
+++ Game/main.cpp
@@ -12,13 +12,13 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 GLFWwindow* window;
 
-bool Init() {
+bool Init(u32 width, u32 height) {
 	if (!glfwInit()){
 		printf("Couldn't initialize GLFW");
 		return false;
 	}
 
-	window = glfwCreateWindow(800, 600, "Game desu", NULL, NULL);
+	window = glfwCreateWindow(width, height, "Game desu", NULL, NULL);
 	if (!window){
 		glfwTerminate();
 		return false;
@@ -34,20 +34,53 @@ bool Init() {
 	}
 }
 
+void print_log(GLuint object)
+{
+	GLint log_length = 0;
+	if (glIsShader(object))
+		glGetShaderiv(object, GL_INFO_LOG_LENGTH, &log_length);
+	else if (glIsProgram(object))
+		glGetProgramiv(object, GL_INFO_LOG_LENGTH, &log_length);
+	else {
+		fprintf(stderr, "printlog: Not a shader or a program\n");
+		return;
+	}
+
+	char* log = (char*)malloc(log_length);
+
+	if (glIsShader(object))
+		glGetShaderInfoLog(object, log_length, NULL, log);
+	else if (glIsProgram(object))
+		glGetProgramInfoLog(object, log_length, NULL, log);
+
+	fprintf(stderr, "%s", log);
+	free(log);
+}
+
 int main(int argc, char** argv) {
-	
-	Init();
+	u32 width = 800, height = 600;
+
+	Init(width, height);
 
 	resource::add_image_png("grass.png");
 	resource::add_shader("vertex.vs");
 	resource::add_shader("fragment.fs");
 	resource::loading_start();
 
+	float projection[] = {
+		2/(float)width, 0, 0, -1,
+		0, 2/(float)height, 0, -1,
+		0, 0, -2, 1,
+		0, 0, 0, 1
+	};
 
 	resource::image_data* grass = resource::get_image("grass.png");
 	GLuint tex_id;
 	glGenTextures(1, &tex_id);
 	glBindTexture(GL_TEXTURE_2D, tex_id);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, grass->x, grass->y, 0, GL_RGBA, GL_UNSIGNED_BYTE, grass->bytes);
 
 	resource::shader_data* vertex = resource::get_shader("vertex.vs");
@@ -57,6 +90,7 @@ int main(int argc, char** argv) {
 	GLuint v_id = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(v_id, 1, &vcode, NULL);
 	glCompileShader(v_id);
+	print_log(v_id);
 
 
 	const char* fcode = fragment->code.c_str();
@@ -64,21 +98,44 @@ int main(int argc, char** argv) {
 	f_id = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(f_id, 1, &fcode, NULL);
 	glCompileShader(f_id);
+	print_log(f_id);
 
 	GLuint program = glCreateProgram();
 	glAttachShader(program, v_id);
 	glAttachShader(program, f_id);
 	glLinkProgram(program);
+	print_log(program);
 
 	GLint attrib_coords = glGetAttribLocation(program, "coords");
+	GLint attrib_color = glGetAttribLocation(program, "color");
+	GLint attrib_texcoords = glGetAttribLocation(program, "texcoords");
 
+	GLint uniform_texture = glGetUniformLocation(program, "texture");
+	GLint uniform_projection = glGetUniformLocation(program, "projection");
 
 	glfwSetKeyCallback(window, key_callback);
 
-	static GLfloat quad[] = { -0.25, -0.25, 0.25, -0.25, 0.25, 0.25, -0.25, 0.25,
-								-.75, -.75, -.5, -.75, -.5, -.5, -.75, -.5,
-								-1, 1, -.75, 1, -.75, .75, -1, .75 };
+	//static GLfloat quad[] = { -0.25, -0.25, 0.25, -0.25, 0.25, 0.25, -0.25, 0.25 };
+	static GLfloat quad[] = { 0, 0, 50, 0, 50, 50, 0, 50 };
+	static GLfloat color[] = { 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
+	static GLfloat texcoords[] = { 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0 };
 
+	GLuint vertex_buffer;
+	glGenBuffers(1, &vertex_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quad), quad, GL_DYNAMIC_DRAW);
+
+	GLuint color_buffer;
+	glGenBuffers(1, &color_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_DYNAMIC_DRAW);
+
+	GLuint texcoords_buffer;
+	glGenBuffers(1, &texcoords_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, texcoords_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(texcoords), texcoords, GL_DYNAMIC_DRAW);
+	
+	glActiveTexture(GL_TEXTURE0);
 
 	while (!glfwWindowShouldClose(window)) {
 
@@ -86,17 +143,44 @@ int main(int argc, char** argv) {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glfwPollEvents();
 
+		//	specify shader program
 		glUseProgram(program);
+
+		//	bind texture
+		glBindTexture(GL_TEXTURE_2D, tex_id);
+		glUniform1i(uniform_texture, 0);
+
+		glUniformMatrix4fv(uniform_projection, 1, GL_TRUE, projection);
+
+		//	upload vertex attribute
+		glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
 		glEnableVertexAttribArray(attrib_coords);
+		glVertexAttribPointer(attrib_coords, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-		glVertexAttribPointer(attrib_coords, 2, GL_FLOAT, GL_FALSE, 0, quad);
+		//	upload texture coordinates
+		glBindBuffer(GL_ARRAY_BUFFER, texcoords_buffer);
+		glEnableVertexAttribArray(attrib_texcoords);
+		glVertexAttribPointer(attrib_texcoords, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
-		glDrawArrays(GL_QUADS, 0, 12);
+		//	upload color attribute
+		glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+		glEnableVertexAttribArray(attrib_color);
+		glVertexAttribPointer(attrib_color, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+		//	draw
+		glDrawArrays(GL_QUADS, 0, 4);
+		
+		//	disable attribs
+		glDisableVertexAttribArray(attrib_color);
+		glDisableVertexAttribArray(attrib_texcoords);
 		glDisableVertexAttribArray(attrib_coords);
 
 		glfwSwapBuffers(window);
 	}
+	//	free up gl resources
 	glDeleteProgram(program);
+	glDeleteTextures(1, &tex_id);
+	
 	resource::destroy();
 	glfwTerminate();
 	return 0;
